400a663b13a1530c210816c6375bd2f1965bb7fa
Kevin Hausmann <kevin@podcatcher-deluxe.com>
2015-10-18 13:36:56 +0200

    Add media type checks to SuggestionTester

--- app/src/androidTest/java/com/podcatcher/deluxe/model/test/SuggestionTester.java
+++ app/src/androidTest/java/com/podcatcher/deluxe/model/test/SuggestionTester.java
@@ -21,6 +21,7 @@ package com.podcatcher.deluxe.model.test;
 import com.podcatcher.deluxe.listeners.OnLoadPodcastListener;
 import com.podcatcher.deluxe.model.tasks.remote.LoadPodcastTask;
 import com.podcatcher.deluxe.model.types.Episode;
+import com.podcatcher.deluxe.model.types.MediaType;
 import com.podcatcher.deluxe.model.types.Podcast;
 import com.podcatcher.deluxe.model.types.Progress;
 import com.podcatcher.deluxe.model.types.Suggestion;
@@ -55,6 +56,10 @@ public class SuggestionTester extends InstrumentationTestCase implements OnLoadP
      * Age of latest podcast episode to trigger warning. (Give -1 to disable warning.)
      */
     private static final int WARN_RECENT_DAYS = 365;
+    /**
+     * Enable/disable media type warnings for suggestions tested.
+     */
+    private static final boolean CHECK_MEDIA_TYPE = true;
 
     public final void testSuggestions() {
         final List<Suggestion> suggestions = Utils.getExamplePodcasts(getInstrumentation().getTargetContext());
@@ -78,7 +83,12 @@ public class SuggestionTester extends InstrumentationTestCase implements OnLoadP
         for (Suggestion suggestion : suggestions)
             for (Map.Entry<String, String> entry : suggestion.getFeeds().entrySet()) {
                 final String name = suggestion.getName() + " (" + entry.getKey() + ")";
-                final Podcast podcast = new Podcast(name, entry.getValue());
+
+                // We make the podcast a suggestion because we want some
+                // of the suggestion properties to be available in onPodcastLoaded()
+                final Suggestion podcast = new Suggestion(name, entry.getValue());
+                podcast.setMediaTypes(suggestion.getMediaTypes());
+
                 new LoadPodcastTask(this).executeOnExecutor(executor, podcast);
             }
 
@@ -101,19 +111,45 @@ public class SuggestionTester extends InstrumentationTestCase implements OnLoadP
         Log.i(TAG, "Podcast " + podcast.getName() + " loaded");
         signal.countDown();
 
-        if (podcast.getEpisodeCount() > 0) {
-            final List<Episode> episodes = podcast.getEpisodes();
+        // This should work and allows access to the original properties
+        // such as media type, language, explicit etc.
+        Suggestion suggestion = (Suggestion) podcast;
+
+        if (suggestion.getEpisodeCount() > 0) {
+            final List<Episode> episodes = suggestion.getEpisodes();
             Collections.sort(episodes);
 
+            // 1. Check episode age
             long ageInDays = 0;
             // Negative age might happen, just invert those (too far in the future is also strange...)
             if (episodes.get(0).getPubDate() != null)
                 ageInDays = Math.abs((new Date().getTime() - episodes.get(0).getPubDate().getTime()) / TimeUnit.DAYS.toMillis(1));
 
             if (WARN_RECENT_DAYS > 0 && ageInDays > WARN_RECENT_DAYS)
-                Log.w(TAG, "Podcast " + podcast.getName() + " latest episode is " + ageInDays + " days old.");
+                Log.w(TAG, "Podcast " + suggestion.getName() + " latest episode is " + ageInDays + " days old.");
+
+            // 2. Check for media types
+            if (CHECK_MEDIA_TYPE)
+                for (int i = 0; i < episodes.size(); i++) {
+                    final Episode episode = episodes.get(i);
+                    MediaType mediaType = null;
+
+                    try {
+                        mediaType = MediaType.forMimeType(episode.getMediaType());
+                    } catch (RuntimeException iae) {
+                        Log.w(TAG, "Podcast " + suggestion.getName() + " has bad media type: " +
+                                (episode.getMediaType() == null ? "null" : episode.getMediaType()) +
+                                " (" + episode.getName() + " [" + (i + 1) + "/" + episodes.size() + "])");
+                    }
+
+                    if (MediaType.VIDEO == mediaType && suggestion.getMediaTypes() != null &&
+                            suggestion.getMediaTypes().size() == 1 && suggestion.getMediaTypes().contains(MediaType.AUDIO))
+                        Log.w(TAG, "Podcast " + suggestion.getName() + " has video content and is marked as audio: (" +
+                                episode.getName() + " [" + (i + 1) + "/" + episodes.size() + "])");
+                }
+
         } else
-            Log.w(TAG, "Podcast " + podcast.getName() + " has no episodes.");
+            Log.w(TAG, "Podcast " + suggestion.getName() + " has no episodes.");
     }
 
     @Override
